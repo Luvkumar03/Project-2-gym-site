80178e06805d9f77f0b6ea5326d9d797f0e8b173
LawKumar <luv19641@gmail.com>
2023-04-25 12:15:45 +0530

    delet comments

--- src/Atom/atom.js
+++ src/Atom/atom.js
@@ -4,13 +4,11 @@ export const Data = atom({
     key : 'Data' ,
     default : false
 })
-
 export const Data2 = atom({
     key : "sub",  
     default: false 
      
   })
-  
   export const Data3 = atom({
     key : "sub1",
     default: false 
